7055bf7f637491162a944a2750a66d66d71cbd5e
maxnevans <maxnevans@gmail.com>
2018-12-10 21:37:31 +0300

    BaseWindow Class Deprecated

--- TestWinapiWindowRedraw/MainWindow.cpp
+++ TestWinapiWindowRedraw/MainWindow.cpp
@@ -1,9 +1,8 @@
 #include "MainWindow.h"
 
 MainWindow::MainWindow(HINSTANCE hInstance, int nCmdShow)
-	:
-	BaseWindow()
 {
+	this->hWnd = NULL;
 
 	this->wWidth = WND_INIT_WIDTH;
 	this->wHeight = WND_INIT_HEIGHT;
@@ -16,18 +15,17 @@ MainWindow::MainWindow(HINSTANCE hInstance, int nCmdShow)
 	this->dWidth = rect.right;
 	this->dHeight = rect.bottom;
 
-	CWS cws = { 0 };
-	cws.lpClassName = CLASS_NAME;
-	cws.lpWindowName = WND_NAME;
-	cws.dwStyle = WND_STYLE;
-	cws.x = (this->dWidth - this->wWidth) / 2;
-	cws.y = (this->dHeight - this->wHeight) / 2;
-	cws.nWidth = WND_INIT_WIDTH;
-	cws.nHeight = WND_INIT_HEIGHT;
-	cws.hWndParent = NULL;
-	cws.hInstance = hInstance;
-	cws.lpParam = 0;
-	this->ResetCWS(cws);
+	this->cws = { 0 };
+	this->cws.lpClassName = CLASS_NAME;
+	this->cws.lpWindowName = WND_NAME;
+	this->cws.dwStyle = WND_STYLE;
+	this->cws.x = (this->dWidth - this->wWidth) / 2;
+	this->cws.y = (this->dHeight - this->wHeight) / 2;
+	this->cws.nWidth = WND_INIT_WIDTH;
+	this->cws.nHeight = WND_INIT_HEIGHT;
+	this->cws.hWndParent = NULL;
+	this->cws.hInstance = hInstance;
+	this->cws.lpParam = 0;
 }
 
 int MainWindow::Register(HINSTANCE hInstance) {
@@ -52,4 +50,36 @@ int MainWindow::WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
 		return DefWindowProc(hWnd, uMsg, wParam, lParam);
 	}
 	return 0;
+}
+
+LRESULT CALLBACK MainWindow::internal_WndProc(HWND hWnd, int msg, WORD wParam, LONG lParam) {
+	MainWindow *c = (MainWindow *)GetWindowLong(hWnd, GWLP_USERDATA);
+
+	if (c == NULL)
+		return DefWindowProc(hWnd, msg, wParam, lParam);
+
+	return c->WndProc(hWnd, msg, wParam, lParam);
+}
+
+void MainWindow::CreateWnd() {
+	this->hWnd = CreateWindow(
+		this->cws.lpClassName,
+		this->cws.lpWindowName,
+		this->cws.dwStyle,
+		this->cws.x, this->cws.y,
+		this->cws.nWidth, this->cws.nHeight,
+		this->cws.hWndParent,
+		this->cws.hMenu,
+		this->cws.hInstance,
+		this->cws.lpParam
+	);
+	if (this->hWnd == NULL) {
+		wchar_t strBuf[256];
+		DWORD err = GetLastError();
+		wsprintf(strBuf, L"Could not create window [%s] '%s'. Error: %d.", this->cws.lpClassName, this->cws.lpWindowName, err);
+		MessageBox(NULL, strBuf, L"Create Window Error", MB_OK | MB_ICONERROR);
+		return;
+	}
+	SetWindowLong(this->hWnd, GWLP_USERDATA, (long)this);
+	UpdateWindow(this->hWnd);
 }
--- TestWinapiWindowRedraw/MainWindow.h
+++ TestWinapiWindowRedraw/MainWindow.h
@@ -1,15 +1,30 @@
 #pragma once
 #include <windows.h>
 #include <gdiplus.h>
-#include "BaseWindow.h"
 
-class MainWindow : public BaseWindow {
+class MainWindow {
+public:
+	typedef struct _CreateWindowStruct {
+		LPCTSTR lpClassName;
+		LPCTSTR lpWindowName;
+		DWORD dwStyle;
+		int x;
+		int y;
+		int nWidth;
+		int nHeight;
+		HWND hWndParent;
+		HMENU hMenu;
+		HINSTANCE hInstance;
+		LPVOID lpParam;
+	} CWS, *PCWS;
 public:
 	MainWindow(HINSTANCE hInstance, int nCmdShow);
 	static int Register(HINSTANCE hInstance);
+	void CreateWnd();
 
 protected:
 	int WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+	static LRESULT CALLBACK internal_WndProc(HWND hWnd, int msg, WORD wParam, LONG lParam);
 
 private:
 	int wWidth;
@@ -20,10 +35,13 @@ private:
 	int cHeight;
 
 	int nCmdShow;
+	CWS cws;
+	HWND hWnd;
+	HINSTANCE hInstance;
 
-	static constexpr wchar_t CLASS_NAME[]	= L"MainWindow";
-	static constexpr wchar_t WND_NAME[]		= L"USB Monitor";
+	static constexpr wchar_t CLASS_NAME[]	= L"TestClass";
+	static constexpr wchar_t WND_NAME[]		= L"Window Title Here";
 	static constexpr int WND_INIT_WIDTH		= 1280;
 	static constexpr int WND_INIT_HEIGHT	= 720;
-	static constexpr DWORD WND_STYLE		= WS_OVERLAPPEDWINDOW | WS_VISIBLE & ~(WS_MAXIMIZEBOX);
+	static constexpr DWORD WND_STYLE		= WS_OVERLAPPEDWINDOW | WS_VISIBLE;
 };
